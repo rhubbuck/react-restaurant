294a086e7e276964d1795d925bb84fe295a61de4
Ryan Hubbuck <rhubbuck@gmail.com>
2021-10-01 19:11:18 -0400

    Installed react-router-dom and styled-components

---
+++ src/Components/Navbar/NavbarElements.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Nav = styled.nav`
+    background: #000;
+    height: 80px;
+`
+
+export const NavbarContainer = styled.div`
+    display: flex;
+    height: 80px;
+`
+
+export const NavLogo = styled.div`
+    color: #fff;
+    cursor: pointer;
+`
